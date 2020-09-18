08bf54339ad615467215370127d287be5d43b1c5
Yuriy Rybin <jura0633728096@gmail.com>
2020-09-18 19:15:43 +0300

    Add message component

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
@@ -298,6 +298,7 @@ module.exports = function(webpackEnv) {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         ...(modules.webpackAliases || {}),
+        styles: path.join(paths.appSrc, "styles")
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import {Message} from "components";
+
+import './Home.scss'
+
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <section className={'home'}>
+            <Message
+                avatar={'https://lh3.googleusercontent.com/868ViUqJ23esYhJVYderhwRFqYT4kTKpzPWSApRX3Pi7dP6SzdDaorbV7vi4bUwwpH9Fci0=s85'}
+                text={'Главный герой — 27-летний Илья Горюнов.'}
+                date={new Date(2020, 6, 2)}
+                isMe={false}
+            />
+            <Message
+                avatar={'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKr-4_3JHSaiKkrTwXGXdRXkpl5dl2o7EaGg&usqp=CAU'}
+                text={'Hello World'}
+                date={new Date(2020, 8, 2)}
+                isMe={true}
+            />
+        </section>
     );
 };
 
